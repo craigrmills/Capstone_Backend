1933026903c604de965a36feb50cb57193173a9a
craigrmills <craig.r.mills86@gmail.com>
2021-09-10 15:32:00 -0500

    Added Category Model

--- eCommerceStarterCode/Models/Category.cs
+++ eCommerceStarterCode/Models/Category.cs
@@ -6,14 +6,9 @@ using System.Threading.Tasks;
 
 namespace BattleSmithAPI.Models
 {
-    public class Product
+    public class Category
     {
-        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
-
-        [ForeignKey("Category")]
-        public string Category { get; set; }
-        public Category Category1 { get; set; }
     }
 }

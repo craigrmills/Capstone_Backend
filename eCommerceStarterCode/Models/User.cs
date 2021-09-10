1369c369872cb38133a89ffb26c65bc51c3f9e8b
craigrmills <craig.r.mills86@gmail.com>
2021-09-10 15:27:52 -0500

    Added Store Model + Modified User Model

--- eCommerceStarterCode/Models/User.cs
+++ eCommerceStarterCode/Models/User.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BattleSmithAPI.Models
 {
-    public class User : IdentityUser
+    public class User
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [ForeignKey("Store")]
+        public string HomeStore { get; set; }
+        public Store Store { get; set; }
+
+        public string Role { get; set; }
     }
 }

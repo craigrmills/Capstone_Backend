cb11a050af967af0e97e44337e5b735b886654ff
craigrmills <craig.r.mills86@gmail.com>
2021-09-10 15:30:09 -0500

    Added Product Model

---
+++ eCommerceStarterCode/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleSmithAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        [ForeignKey("Category")]
+        public string Category { get; set; }
+        public Category Category { get; set; }
+    }
+}

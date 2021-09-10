83d19bc52a36113dbaa5175acd8d337d797b2b23
craigrmills <craig.r.mills86@gmail.com>
2021-09-10 14:15:27 -0500

    Added Faction Model

---
+++ eCommerceStarterCode/Models/Faction.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BattleSmithAPI.Models
+{
+    public class Faction
+    {
+        public int FactionId { get; set; }
+        public string Name { get; set; }
+        public int GamesPlayed { get; set; }
+        public int WinRate { get; set; }
+    }
+}

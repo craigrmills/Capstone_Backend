629dc8446971a0d34bcbb95ec599ee651f4340a1
craigrmills <craig.r.mills86@gmail.com>
2021-09-10 15:36:42 -0500

    Added Order Model

---
+++ eCommerceStarterCode/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleSmithAPI.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}

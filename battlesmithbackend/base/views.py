c51dff73a796aef74c9d79558819a099a0b496c5
craigrmills <craig.r.mills86@gmail.com>
2021-09-14 13:35:29 -0500

    Added SimpleJWT and set up Token Customization

--- battlesmithbackend/base/views.py
+++ battlesmithbackend/base/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import update_last_login
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import serializers
@@ -9,8 +10,24 @@ from .models import Product
 
 from .products import products
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
